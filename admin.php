704997152ec2327cb5602704281d3ca4b24cf6fe
Satoshi Sahara <sahara.satoshi@gmail.com>
2015-10-06 21:52:41 +0900

    code clean-up

--- admin.php
+++ admin.php
@@ -13,71 +13,9 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
 
     protected $logFile = 'loglog.log'; // stored in cache directory
 
-    protected $table = array();
+    protected $props = array();        // hold table propaties
     protected $term_default = 'monthly';
 
-
-    /**
-     * Resolve 'time' url parameter
-     *
-     * @param string $request   requested start date of log table
-     * @param array  $data      reference, log table parameters
-     * @return string           verified start date notations
-     */
-    function resolve_time($request, &$data) {
-
-        // simple check url paramater 'time'
-        if (strpos($request, 'W') !== false) {
-            // ISO year with ISO week;              eg. 2015W04
-            $term = 'weekly';
-        } else if (strlen($request) == 8) {
-            // Eight digit year, month and day;     eg. 20150207
-            $term = 'daily';
-        } else if (strlen($request) == 6) {
-            // Four digit year and two digit month; eg. 201504
-            $term = 'monthly'; $request .= '01';
-        } else {
-            $term = $this->term_default;
-        }
-
-        // rebuild 'time' parameter and set it to $go
-        $time = strtotime($request) ?: time();
-        switch ($term) {
-            case 'daily':
-                $go  = strftime('%Y%m%d', $time);
-                $min = strtotime('today 00:00:00', $time);
-                $max = strtotime('today 23:59:59', $time);
-                $forward  = strftime('%Y%m%d', strtotime('+1 day', $min));
-                $backward = strftime('%Y%m%d', strtotime('-1 day', $min));
-                $caption  = strftime('%c', $time);
-                break;
-            case 'monthly':
-                $go  = strftime('%Y%m', $time);
-                $min = strtotime('first day of this month 00:00:00', $time);
-                $max = strtotime('last day of this month 23:59:59', $time);
-                $forward  = strftime('%Y%m', strtotime('+1 month', $min));
-                $backward = strftime('%Y%m', strtotime('-1 month', $min));
-                $caption  = strftime('%B %Y', $time);
-                break;
-            case 'weekly':
-                $go  = strftime('%YW%V', $time);
-                $n = date('N', $time)-1; // 0:monday ... 6:sunday
-                $min = strtotime("-{$n} day 00:00:00", $time);
-                $max = strtotime('sunday 23:59:59', $time);
-                $forward  = strftime('%YW%V', strtotime('+1 week', $min));
-                $backward = strftime('%YW%V', strtotime('-1 week', $min));
-                $caption  = strftime('Week %V of %Y year', $time);
-                break;
-        }
-        $data = array(
-            'caption'  => $caption,  // table caption
-            'min'      => $min,
-            'max'      => $max,
-            'forward'  => $forward,  // time request of newer button
-            'backward' => $backward, // time request of older button
-        );
-        return $go;
-    }
 
     /**
      * Access for managers allowed
@@ -95,8 +33,58 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
     function handle() {
         global $INPUT;
 
-        $go = $this->resolve_time($INPUT->str('time'), $this->table);
-        // error_log(var_export($this->table, true));
+        $request = $INPUT->str('time');  // $_REQUEST['time']
+
+        // simple check url paramater 'time'
+        if (strpos($request, 'W') !== false) {
+            // ISO year with ISO week;              eg. 2015W04
+            $term = 'weekly';
+        } else if (strlen($request) == 8) {
+            // Eight digit year, month and day;     eg. 20150207
+            $term = 'daily';
+        } else if (strlen($request) == 6) {
+            // Four digit year and two digit month; eg. 201504
+            $term = 'monthly'; $request .= '01';
+        } else {
+            $term = $this->term_default;
+        }
+
+        // set table parameters
+        $time = strtotime($request) ?: time();
+        switch ($term) {
+            case 'daily':
+                $request = strftime('%Y%m%d', $time);
+                $min = strtotime('today 00:00:00', $time);
+                $max = strtotime('today 23:59:59', $time);
+                $forward  = strftime('%Y%m%d', strtotime('+1 day', $min));
+                $backward = strftime('%Y%m%d', strtotime('-1 day', $min));
+                $caption  = strftime('%c', $time);
+                break;
+            case 'monthly':
+                $request = strftime('%Y%m', $time);
+                $min = strtotime('first day of this month 00:00:00', $time);
+                $max = strtotime('last day of this month 23:59:59', $time);
+                $forward  = strftime('%Y%m', strtotime('+1 month', $min));
+                $backward = strftime('%Y%m', strtotime('-1 month', $min));
+                $caption  = strftime('%B %Y', $time);
+                break;
+            case 'weekly':
+                $request = strftime('%YW%V', $time);
+                $n = date('N', $time)-1; // 0:monday ... 6:sunday
+                $min = strtotime("-{$n} day 00:00:00", $time);
+                $max = strtotime('sunday 23:59:59', $time);
+                $forward  = strftime('%YW%V', strtotime('+1 week', $min));
+                $backward = strftime('%YW%V', strtotime('-1 week', $min));
+                $caption  = strftime('Week %V of %Y year', $time);
+                break;
+        }
+        $this->props = array(
+            'caption'  => $caption,  // table caption
+            'min'      => $min,
+            'max'      => $max,
+            'forward'  => $forward,  // time request of newer button
+            'backward' => $backward, // time request of older button
+        );
     }
 
     /**
@@ -105,23 +93,21 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
     function html() {
         global $ID, $conf, $lang;
 
-        // login/logout table
-        // pagenation based on monthly, weekly or daily 
-
-        $min = $this->table['min'];
-        $max = $this->table['max'];
+        // render login/logout table based in time request
+        // with monthly, weekly or daily pagenation
 
         echo '<h1>'.$this->getLang('menu').'</h1>';
         echo '<div class="loglog_noprint">';
         echo $this->locale_xhtml('intro');
 
         echo '<p>'.$this->getLang('range').' '.
-             strftime('%F (%a)',$min).' - '.strftime('%F (%a)',$max).'</p>';
+             strftime('%F (%a)',$this->props['min']).' - '.
+             strftime('%F (%a)',$this->props['max']).'</p>';
         echo '</div>';
 
 
         echo '<table class="inline loglog">';
-        echo '<caption>',$this->table['caption'].'</caption>';
+        echo '<caption>',$this->props['caption'].'</caption>';
         echo '<tr>';
         echo '<th>'.$this->getLang('date').'</th>';
         echo '<th>'.$this->getLang('ip').'</th>';
@@ -129,40 +115,40 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
         echo '<th>'.$this->getLang('action').'</th>';
         echo '</tr>';
 
-        $lines = $this->_readlines($min,$max);
+        $lines = $this->_readlines($this->props['min'],$this->props['max']);
         $lines = array_reverse($lines);
 
         foreach($lines as $line){
             if (empty($line)) continue; // Filter empty lines
-            list($dt,$junk,$ip,$user,$msg) = explode("\t",$line,5);
-            if($dt < $min) continue;
-            if($dt > $max) continue;
-            if(!$user)     continue;
+            list($dt, $junk, $ip, $user, $msg) = explode("\t", $line, 5);
+            if ($dt < $this->props['min']) continue;
+            if ($dt > $this->props['max']) continue;
+            if (!$user) continue;
 
-            if($msg == 'logged off'){
+            if ($msg == 'logged off'){
                 $msg = $this->getLang('off');
                 $class = 'off';
-            }elseif($msg == 'logged in permanently'){
+            } elseif ($msg == 'logged in permanently'){
                 $msg = $this->getLang('in');
                 $class = 'perm';
-            }elseif($msg == 'logged in temporarily'){
+            } elseif ($msg == 'logged in temporarily'){
                 $msg = $this->getLang('tin');
                 $class = 'temp';
-            }elseif($msg == 'failed login attempt'){
+            } elseif ($msg == 'failed login attempt'){
                 $msg = $this->getLang('fail');
                 $class = 'fail';
-            }elseif($msg == 'has been automatically logged off') {
+            } elseif ($msg == 'has been automatically logged off') {
                 $msg = $this->getLang('autologoff');
                 $class = 'off';
-            }else{
+            } else {
                 $msg = hsc($msg);
-                if(strpos($msg, 'logged off') !== false) {
+                if (strpos($msg, 'logged off') !== false) {
                     $class = 'off';
-                } elseif(strpos($msg, 'logged in permanently') !== false) {
+                } elseif (strpos($msg, 'logged in permanently') !== false) {
                     $class = 'perm';
-                } elseif(strpos($msg, 'logged in') !== false) {
+                } elseif (strpos($msg, 'logged in') !== false) {
                     $class = 'temp';
-                } elseif(strpos($msg, 'failed') !== false) {
+                } elseif (strpos($msg, 'failed') !== false) {
                     $class = 'fail';
                 } else {
                     $class = 'unknown';
@@ -180,14 +166,14 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
         echo '</table>';
 
         echo '<div class="pagenav loglog_noprint">';
-        if($max < time()){
+        if ($this->props['max'] < time()){
         echo '<div class="pagenav-prev">';
-        echo html_btn('newer',$ID,"p",array('do'=>'admin','page'=>'loglog','time'=>$this->table['forward']));
+        echo html_btn('newer',$ID,"p",array('do'=>'admin','page'=>'loglog','time'=>$this->props['forward']));
         echo '</div>';
         }
 
         echo '<div class="pagenav-next">';
-        echo html_btn('older',$ID,"n",array('do'=>'admin','page'=>'loglog','time'=>$this->table['backward']));
+        echo html_btn('older',$ID,"n",array('do'=>'admin','page'=>'loglog','time'=>$this->props['backward']));
         echo '</div>';
         echo '</div>';
 
@@ -221,126 +207,41 @@ class admin_plugin_loglog extends DokuWiki_Admin_Plugin {
         while($pos){
 
             // how much to read? Set pointer
-            if($pos > $chunk_size){
+            if ($pos > $chunk_size) {
                 $pos -= $chunk_size;
                 $read = $chunk_size;
-            }else{
+            } else {
                 $read = $pos;
                 $pos  = 0;
             }
             fseek($fp,$pos);
 
             $tmp = fread($fp,$read);
-            if($tmp === false) break;
+            if ($tmp === false) break;
             $chunk = $tmp.$chunk;
 
             // now split the chunk
             $cparts = explode("\n",$chunk);
 
             // keep the first part in chunk (may be incomplete)
-            if($pos) $chunk = array_shift($cparts);
+            if ($pos) $chunk = array_shift($cparts);
 
             // no more parts available, read on
-            if(!count($cparts)) continue;
+            if (!count($cparts)) continue;
 
             // get date of first line:
             list($cdate) = explode("\t",$cparts[0]);
 
-            if($cdate > $max) continue; // haven't reached wanted area, yet
+            if ($cdate > $max) continue; // haven't reached wanted area, yet
 
             // put the new lines on the stack
             $lines = array_merge($cparts,$lines);
 
-            if($cdate < $min) break; // we have enough
+            if ($cdate < $min) break; // we have enough
         }
         fclose($fp);
 
         return $lines;
     }
-
-    /**
-     * convert 1,2,3,4 to 1st, 2nd, 3rd, 4th etc
-     */
-    function ordSuffix($n) {
-        $str = "$n";
-        $t = ($n > 9) ? substr($str,-2,1) : 0;
-        $u = substr($str,-1);
-        if ($t==1) return $str . 'th';
-        else switch ($u) {
-            case 1: return $str . 'st';
-            case 2: return $str . 'nd';
-            case 3: return $str . 'rd';
-            default: return $str . 'th';
-        }
-    }
-
-    /**
-     * get time range of the log table
-     *
-     * @param string $term   unit fo date range of table (month, week, or day)
-     * @param int    $time   timestamp of interest time
-     * @return array min and max timestamp of the table
-     */
-    function getRange($term = 'week', $time = 0) {
-        if (!$time) $time = time();
-        switch ($term) {
-            case 'day':
-                $min = strtotime('today 00:00:00', $time);
-                $max = strtotime('today 23:59:59', $time);
-                break;
-            case 'month':
-                $min = strtotime('first day of this month 00:00:00', $time);
-                $max = strtotime('last day of this month 23:59:59', $time);
-                break;
-            case 'week0': // week number, 0(for Sunday) through 6(for Saturday)
-                $w = date('w', $time); // 0:Sunday ... 6:Saturday
-                $min = strtotime("-{$w} day 00:00:00", $time);
-                $max = strtotime('saturday 23:59:59', $time);
-                break;
-            case 'week':  // ISO-8601:1999 week number, 1(for Monday) through 7(for Sunday)
-            default:
-                $w = date('N', $time)-1; // 0:monday ... 6:sunday
-                $min = strtotime("-{$w} day 00:00:00", $time);
-                $max = strtotime('sunday 23:59:59', $time);
-        }
-        return array($min, $max);
-    }
-
-    /**
-     * get timestamp of adjacent date
-     *
-     * @param int $relative  +1 (forward) or -1 (backward)
-     * @param string $term   unit fo date range of table (month, week, or day)
-     * @param int    $time   timestamp of interest time
-     * @return int   timestamp
-     *
-     */
-    function adjacent_date($relative, $term, $time) {
-        $format = $relative.' '.rtrim($term,'0').' 00:00:00';
-        return strtotime($format , $time);
-    }
-
-    /**
-     * get appropriate table caption
-     *
-     * @param string $term   unit fo date range of table (month, week, or day)
-     * @param int    $time   timestamp of interest time
-     * @return sring         table caption
-     */
-    function table_caption($term, $time) {
-        switch ($term) {
-            case 'day':
-                $format = '%c'; break;
-            case 'month':
-                $format = '%B %Y'; break;
-            case 'week0':
-                $format = 'Week %U of %Y year (Sunday - Saturday)'; break;
-            case 'week':
-            default:
-                $format = 'Week %V of %Y year';
-        }
-        return strftime($format, $time);
-    }
-
 
 }
